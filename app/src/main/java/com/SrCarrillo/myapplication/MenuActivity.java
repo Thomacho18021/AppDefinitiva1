3694b70754d9962e40bd0bf78cc500f0d9cf857e
SrCarrillo <thomascarrillogonzales@gmail.com>
2020-11-26 01:21:33 -0300

    Commit 3

--- app/src/main/java/com/SrCarrillo/myapplication/MenuActivity.java
+++ app/src/main/java/com/SrCarrillo/myapplication/MenuActivity.java
@@ -2,7 +2,6 @@ package com.SrCarrillo.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -10,21 +9,27 @@ import android.widget.ImageButton;
 
 public class MenuActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private ImageButton btn1, btn2, btn3, btn4, btn5 ,btn6;
+    private ImageButton btnRadio, btn2, btn3, btn4, btn5 ,btn6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-        btn1 = findViewById(R.id.btn1);
+        btnRadio = findViewById(R.id.btnRadio);
         btn2 = findViewById(R.id.btn2);
         btn3 = findViewById(R.id.btn3);
         btn4 = findViewById(R.id.btn4);
         btn5 = findViewById(R.id.btn5);
         btn6 = findViewById(R.id.btn6);
 
-        btn1.setOnClickListener(this);
+        btnRadio.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+
     }
     private void ir_a(Class c){
         Intent i = new Intent(MenuActivity.this,c);
@@ -34,7 +39,7 @@ public class MenuActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btn1:
+            case R.id.btnRadio:
                 ir_a(RadioActivity.class);
                 break;
             case R.id.btn2:
