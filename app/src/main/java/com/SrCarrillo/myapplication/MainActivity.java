0e9edcf35ead9d35a166dc2feda7f15b6485aba3
SrCarrillo <thomascarrillogonzales@gmail.com>
2020-12-07 00:24:11 -0300

    Coomit 12 : se agrega libro de exodo y levitico para seccion biblia

--- app/src/main/java/com/SrCarrillo/myapplication/MainActivity.java
+++ app/src/main/java/com/SrCarrillo/myapplication/MainActivity.java
@@ -45,17 +45,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
 
 
-    //Declarar objeto FirebaseAuth
-    /*private FirebaseAuth firebaseAuth;
-    FirebaseAuth.AuthStateListener mAuthStateListener;
-    List<AuthUI.IdpConfig> provider = Arrays.asList(
-            new AuthUI.IdpConfig.FacebookBuilder().build(),
-            new AuthUI.IdpConfig.GoogleBuilder().build(),
-            new AuthUI.IdpConfig.EmailBuilder().build(),
-            new AuthUI.IdpConfig.PhoneBuilder().build()
-    );*/
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -66,34 +55,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         database = FirebaseDatabase.getInstance();
         reference = database.getReference();
 
-
-        /*firebaseAuth = FirebaseAuth.getInstance();
-        mAuthStateListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser  user = firebaseAuth.getCurrentUser();
-                if(user!= null){
-                    Toast.makeText(MainActivity.this,"Iniciaste Sesion", Toast.LENGTH_SHORT).show();
-                }else{
-                    startActivityForResult(AuthUI
-                            .getInstance()
-                            .createSignInIntentBuilder()
-                            .setAvailableProviders(provider)
-                            .setIsSmartLockEnabled(false)
-                            .build(),
-                            REQUEST_CODE);
-
-                }
-            }
-        };*/
         txtOlvContra = findViewById(R.id.txtOlvContra);
         txtRegistrate = findViewById(R.id.txtRegistrate);
 
         txtEmail = findViewById(R.id.txtEmail);
         txtContrasena = findViewById(R.id.txtContrasena);
         btnIniciarSesion = findViewById(R.id.btnIniciarSesion);
-        /*btnGoogle = findViewById(R.id.btnGoogle);
-        btnFacebook = findViewById(R.id.btnFacebook);*/
         progressDialog = new ProgressDialog(this);
 
         txtRegistrate.setOnClickListener(this);
@@ -136,103 +103,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         alerta("Iniciando Sesion...");
     }
-
-    /*@Override
-    protected void onStart() {
-        FirebaseUser user = firebaseAuth.getCurrentUser();
-        if(user != null){
-            SharedPreferences prefs = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
-            String password_guardada = prefs.getString("password","");
-            Intent i = new Intent(MainActivity.this,MenuActivity.class);
-            i.putExtra("password",password_guardada);
-            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
-            startActivity(i);
-        }
-        super.onStart();
-    }*/
-
- /*private void RegistrarUsuario() {
-        String email = txtEmail.getText().toString().trim();
-        String contrasena = txtContrasena.getText().toString().trim();
-
-        if (TextUtils.isEmpty(email)) {
-            txtEmail.setError("Casilla Vacia");
-            return;
-        }
-        if (TextUtils.isEmpty(contrasena)) {
-            txtContrasena.setError("Casilla Vacia");
-            return;
-        }
-
-        progressDialog.setMessage("Realizando registro...");
-        progressDialog.show();
-
-
-        //crear nuevo usuario
-        firebaseAuth.createUserWithEmailAndPassword(email, contrasena).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Toast.makeText(MainActivity.this, "Email Registrado", Toast.LENGTH_LONG).show();
-                } else {
-                    if (task.getException() instanceof FirebaseUiUserCollisionException) {
-                        Toast.makeText(MainActivity.this, "Usuario Ya Registrado", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(MainActivity.this, "Registro Fallido", Toast.LENGTH_LONG).show();
-                    }
-                }
-                progressDialog.dismiss();
-            }
-        });
-
-    }*/
-
-   /* private void LoginUsuario() {
-        String email = txtEmail.getText().toString().trim();
-        String contrasena = txtContrasena.getText().toString().trim();
-
-        if (email.isEmpty() ) {
-            showError(txtEmail, "Email inválido");
-            return;
-        }
-        if (contrasena.isEmpty() || contrasena.length()<6) {
-            showError(txtContrasena, "Contraseña inválida");
-            return;
-        }
-
-        progressDialog.setTitle("Login");
-        progressDialog.setMessage("Realizando Login...");
-        progressDialog.show();
-
-        //Inicio de Sesion
-        firebaseAuth.signInWithEmailAndPassword(email, contrasena).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    progressDialog.dismiss();
-
-                    SharedPreferences prefs = getSharedPreferences("MyPreferences", Context.MODE_PRIVATE);
-                    SharedPreferences.Editor editor = prefs.edit();
-                    editor.putString("password",contrasena);
-                    editor.commit();
-                    int pos = email.indexOf("@");
-                    String user = email.substring(0,pos);
-                    Toast.makeText(MainActivity.this, "Bienvenido ", Toast.LENGTH_LONG).show();
-                    Intent intent = new Intent(getApplication(),MenuActivity.class);
-                    intent.putExtra("password",contrasena);
-                    intent.putExtra(RegistrateActivity.usuario,user);
-                    startActivity(intent);
-                } else {
-                    if (task.getException() instanceof FirebaseUiUserCollisionException) {
-                        Toast.makeText(MainActivity.this, "Usuario Ya Registrado", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(MainActivity.this, "Registro Fallido", Toast.LENGTH_LONG).show();
-                    }
-                }
-
-            }
-        });
-    }*/
+    
     private void showError(EditText input, String s){
         input.setError(s);
         input.requestFocus();
